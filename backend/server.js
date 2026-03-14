c7946dd6efed19fdd1233a746181b07ef0036d38
marcosaccarola@gmail.com <marcosaccarola@gmail.com>
2026-03-14 13:38:13 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', corsOrigin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  res.setHeader('Access-Control-Max-Age', '86400');
+  if (req.method === 'OPTIONS') return res.status(204).end();
   next();
 });
 app.use(express.json());
